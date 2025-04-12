934da652a95b8deddd655b619b7794621d27a9a6
DuozhongChen <156970535+DuozhongChen@users.noreply.github.com>
2025-04-12 18:06:39 -0400

    Added axiosClient.js to handle API requests

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -1,10 +1,21 @@
 import axios from 'axios';
 
-const isLocalhost = window.location.hostname === 'localhost';
+// const isLocalhost = window.location.hostname === 'localhost';
+
+// const BASE_URL = isLocalhost
+//   ? 'http://localhost:5000'   // 本地开发
+//   //: 'http://backend:5000';    // Docker 容器内通信
+//   : 'http://47.89.164.212:5000';    // Docker 容器内通信
+
+
+const { hostname, protocol } = window.location;
+
+const isLocalhost = hostname === 'localhost';
 
 const BASE_URL = isLocalhost
-  ? 'http://localhost:5000'   // 本地开发
-  : 'http://backend:5000';    // Docker 容器内通信
+  ? 'http://localhost:5000'
+  : `${protocol}//${hostname}:5000`; // 自动取公网 IP 或域名
+
 
 const axiosClient = axios.create({
   baseURL: BASE_URL,
